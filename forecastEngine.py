51f55436100fda883ee3acb56a2d8f3fadf4c83d
akash.achanta <akash7236@gmail.com>
2020-06-17 20:46:41 +0530

    Changes to retrieve graph data

--- forecastEngine.py
+++ forecastEngine.py
@@ -12,21 +12,21 @@ class forecastEngine:
         filename = 'models/Item_' + str(item) + '_model.sav'
         model = pickle.load(open(filename, 'rb'))
         forecasted_count = np.round(model.forecast(day_count))
-        weekday = datetime.today().weekday()
-        forecast = np.roll(forecasted_count,5-weekday)            
+        weekday = datetime.today().weekday()        
+        forecast = np.roll(forecasted_count,5-weekday-1)            
         #forecast = forecast.astype(str)        
         return [int(count) for count in forecast]
 
     def get_forecast(self):
         dates = []
         Curr_date = datetime.today()
-        wd = Curr_date.weekday()        
+        wd = Curr_date.weekday()            
         for x in range(7):            
-            rem = (wd + x) % 7            
+            rem = (wd + x + 1) % 7            
             if rem in (5, 6):
                 continue
             
-            dates.append(Curr_date + timedelta(days=x))        
+            dates.append(Curr_date + timedelta(days=x + 1))        
             
         
         list_dates = [date.strftime('%Y-%m-%d') for date in dates]
@@ -34,8 +34,11 @@ class forecastEngine:
 
         forecast_data = []
         item_look_up  = pd.read_csv('data/FoodItemLookup.csv')
+        #print("item_look", item_look_up)
         for i in item_look_up.item_id:
-            forecast_data.append({ 'data' : list(self.item_forecast(5, i)), 'label' : list(item_look_up.loc[item_look_up.item_id == i]['item_name'].values)})
-        forecast_data.append({ 'data' : list(self.item_forecast(5, "total_orders")), 'label' : ["total_orders"]})
-        output = list_dates, forecast_data        
+            forecast_data.append({ 'data' : list(self.item_forecast(5, i)), 'label' : item_look_up[item_look_up.item_id == i]['item_name'].values[0]})
+        
+        forecast_data.append({ 'data' : list(self.item_forecast(5, "total_orders")), 'label' : "total_orders"})
+        output = list_dates, forecast_data
+        #print(output)
         return output
