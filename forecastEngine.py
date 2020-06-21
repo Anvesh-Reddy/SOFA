930aece75b0ec33fc08af7d4a96fb9772b26e581
Anvesh Matta <anvesh.matta@ggktech.com>
2020-06-21 12:00:54 +0530

    reverted the date diffs

--- forecastEngine.py
+++ forecastEngine.py
@@ -22,7 +22,7 @@ class forecastEngine:
 
     def get_forecast(self):
         dates = []
-        Curr_date = datetime.today() - timedelta(days=1)
+        Curr_date = datetime.today() #- timedelta(days=1)
         wd = Curr_date.weekday()            
         for x in range(7):            
             rem = (wd + x + 1) % 7            
